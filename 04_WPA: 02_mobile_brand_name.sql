40665a3fad93328a173005df19a9fa83114be75a
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-05-08 17:41:17 +0200

    Delete 04_WPA: 02_mobile_brand_name.sql

--- 04_WPA: 02_mobile_brand_name.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `steam-mantis-108908.WPA_Dimensions.02_mobile_brand_name`
-AS
-SELECT distinct(TRIM(LOWER(mobile_brand_name))) as mobile_brand_name FROM `steam-mantis-108908.WPA_Tables.00_Sessions`
-WHERE mobile_brand_name IS NOT NULL AND mobile_brand_name<>''
-ORDER BY mobile_brand_name ASC
