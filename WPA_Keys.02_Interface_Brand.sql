b4acaeac5ba7464afd6c28f8ff51ff5ec65160ea
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-05-08 17:42:25 +0200

    Delete WPA_Keys.02_Interface_Brand.sql

--- WPA_Keys.02_Interface_Brand.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `steam-mantis-108908.WPA_Keys.02_Interface_Brand` AS
-SELECT DISTINCT
-ABS(MOD(FARM_FINGERPRINT(IFNULL(NULLIF(Interface_Brand, ''), 'Unknown')), 100000)) AS Key_Interface_Brand,
-IFNULL(NULLIF(Interface_Brand, ''), 'Unknown') AS Interface_Brand
-FROM `steam-mantis-108908.WPA.*`
