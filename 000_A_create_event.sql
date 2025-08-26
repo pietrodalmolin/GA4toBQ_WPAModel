2f8a45bc6b1693e94296165013becbb81f01b23c
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-08-26 13:26:39 +0200

    Update 000_A_create_event.sql

--- 000_A_create_event.sql
+++ 000_A_create_event.sql
@@ -70,14 +70,14 @@ AND wpa.geo.country=ca.country
 LEFT JOIN 
 (SELECT session_id,MAX(lnd_source) as lnd_source,MAX(lnd_medium) as lnd_medium,MAX(channel_grouping) as channel_grouping 
 FROM `steam-mantis-108908.WPA_Events.00_LastNonDirectTraffic`
-WHERE date < TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)) --*(4)
+WHERE date < TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)) --*(3)
 GROUP BY session_id) lnd
 ON FARM_FINGERPRINT(CONCAT(wpa.user_pseudo_id, ga_session_id)) = lnd.session_id
 
 --DATE FILTER
-WHERE wpa.date < TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)) --*(4)
+WHERE wpa.date < TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)) --*(3)
 
---EVENT FILTERS *(5)
+--EVENT FILTERS *(4)
 AND Event_Name='Registration_Funnel'    /*EXAMPLE FOR NRC*/
 AND EventAction='NRC'                   /*EXAMPLE FOR NRC*/
 
