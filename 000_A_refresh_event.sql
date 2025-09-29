ee172d7b34d8615d9f591a907a8c3deb57245acb
pietrodalmolin <pietro.dalmolin@betssongroup.com>
2025-09-29 15:50:03 +0200

    Update 000_A_refresh_event.sql

--- 000_A_refresh_event.sql
+++ 000_A_refresh_event.sql
@@ -1,17 +1,74 @@
-INSERT INTO `steam-mantis-108908.WPA_Events.TABLE NAME`
-(
-...
+INSERT INTO `steam-mantis-108908.WPA_Events.WPA_CA_NRC` --*(1)
 
---MAPPING WITH LND TRAFFIC SOURCE
-LEFT JOIN 
-(SELECT session_id,MAX(lnd_source) as lnd_source,MAX(lnd_medium) as lnd_medium,MAX(channel_grouping) as channel_grouping 
-FROM `steam-mantis-108908.WPA_Events.00_LastNonDirectTraffic`
-WHERE date = TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
-GROUP BY session_id) lnd
-ON FARM_FINGERPRINT(CONCAT(wpa.user_pseudo_id, ga_session_id)) = lnd.session_id
+SELECT
+--"SESSION LEVEL" COLUMNS
+property_id
+,date
+,event_datetime
+,event_timestamp
+,session_id
+,user_pseudo_id
+,GUID
+,Key_ga_session_number
+,Key_session_engaged
+,Key_Interface_Brand
+,Key_Customer_Status_Start
+,Key_city
+,Key_country
+,Key_region
+,Key_Commercial_Area
+,Key_Technical_PlatformUsed
+,Key_mobile_brand_name
+,Key_device_web_info_browser
+,Key_device_web_info_browser_version
+,Key_device_category
+,Key_mobile_marketing_name
+,Key_mobile_model_name
+,Key_device_operating_system
+,Key_device_operating_system_version
+,Key_app_info_id
+,Key_app_info_version
+,Key_app_info_install_source
+
+--TRAFFIC COLUMNS
+,lnd_source
+,lnd_medium
+,lc_source
+,lc_medium
+,Key_channel_grouping
+
+--GLOBAL DIMENSIONS
+,Key_device_web_info_hostname
+,Key_Interface_SiteLanguage
+,Key_Jurisdiction
+,content_group
+,event_name
+,Technical_EventName
+,EventAction
+,Key_Customer_Status_Event
+,Key_Login_Status
+,page_location
+,page_referrer
+,page_title
+,Key_Technical_PlatformName
+,Technical_ScreenOrientation
+,Technical_ScreenResolution
+,Key_User_CustomerLevel
+,Sub_Area
+--EVENT SPECIFIC DIMENSIONS *(2)
+,Registration_Type
+,User_Reg_Method
+,User_Reg_Step
+
+--METRICS
+,1 AS Event_Count
+
+FROM `steam-mantis-108908.WPA.00_MasterTable` m
 
 --DATE FILTER
-WHERE wpa.date = TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY))
+WHERE date = TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)) --*(3)
 
-...
-)
+--EVENT FILTERS *(4)
+AND Event_Name='Registration_Funnel'
+AND Technical_EventName='ConfirmedRegistration'
+AND EventAction='NRC'
